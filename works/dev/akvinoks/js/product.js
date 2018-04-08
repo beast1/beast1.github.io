f416b80bcb7926655fc6c55c24c7989096d2b332
Egor Glebov <beast.loby@gmail.com>
2018-04-08 17:58:10 +0300

    upd dev preview

--- works/dev/akvinoks/js/product.js
+++ works/dev/akvinoks/js/product.js
@@ -3,14 +3,23 @@ function initSwiper() {
     navigation: {
       nextEl: '.hello-slider .slider-controls-item_next',
       prevEl: '.hello-slider .slider-controls-item_prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets'
     }
   });
   initCustomPagination('.hello-slider', helloSwiper);
 
   var catalogSwiper = new Swiper('.project__catalog-swiper-container', {
+    spaceBetween: 40,
     navigation: {
       nextEl: '.project__catalog .slider-controls-item_next',
       prevEl: '.project__catalog .slider-controls-item_prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets'
     }
   });
   initCustomPagination('.project__catalog', catalogSwiper);
