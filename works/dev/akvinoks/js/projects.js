c75f28d505249e7408be8b754ab33748bed83505
Egor Glebov <beast.loby@gmail.com>
2018-04-10 20:42:40 +0300

    upd dev preview

--- works/dev/akvinoks/js/projects.js
+++ works/dev/akvinoks/js/projects.js
@@ -1,18 +1,5 @@
-//function initToggleMod(itemClass, itemMod, parent) {
-//  var itemsParent = parent ? document.querySelector(parent) : document;
-//  var items = document.querySelector('.' + itemClass);
-//  console.log(items)
-//  function toggleMod(evt) {
-//    items.forEach(function(item) {
-//      item.classList.remove(itemClass + '_' + itemMod);
-//    });
-//    evt.target.classList.add(itemClass + '_' + itemMod);
-//  }
-//  items.addEventListener('click', toggleMod);
-//}
-//initToggleMod('projects__filters-item', 'current');
 function initMenu() {
-  var items = document.querySelectorAll('.projects__filters-item');
+  var items = Array.from(document.querySelectorAll('.projects__filters-item'));
   items.forEach(function(item) {
     item.addEventListener('click', function() {
       items.forEach(function(it) {
