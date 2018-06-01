ab43aa2bbde3af1a7968144d9d289b8a78b1d9d5
Егор Глебов <beast.loby@gmail.com>
2018-06-01 15:02:04 +0300

    upd dev

--- works/dev/ph_build/js/main.js
+++ works/dev/ph_build/js/main.js
@@ -38,15 +38,8 @@ class AbstractView {
 
 }
 
-const footer = `<footer class="footer">
-    <a href="https://htmlacademy.ru" class="social-link social-link--academy">HTML Academy</a>
-    <span class="footer__made-in">Сделано в <a href="https://htmlacademy.ru" class="footer__link">HTML Academy</a> &copy; 2016</span>
-    <div class="footer__social-links">
-      <a href="https://twitter.com/htmlacademy_ru" class="social-link  social-link--tw">Твиттер</a>
-      <a href="https://www.instagram.com/htmlacademy/" class="social-link  social-link--ins">Инстаграм</a>
-      <a href="https://www.facebook.com/htmlacademy" class="social-link  social-link--fb">Фэйсбук</a>
-      <a href="https://vk.com/htmlacademy" class="social-link  social-link--vk">Вконтакте</a>
-    </div>
+const footer = `
+  <footer class="footer">
   </footer>`;
 
 class GreetingView extends AbstractView {
