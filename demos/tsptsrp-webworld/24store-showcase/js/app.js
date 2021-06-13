b60da620b0460610cf8eeb30498fdcd1d4f3db1f
Orest <obylous@s-pro.io>
2021-06-13 03:56:41 +0300

    add 24store showcase

---
+++ demos/tsptsrp-webworld/24store-showcase/js/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    (function () {
+        var mySwiper = new Swiper('#swiper-root', {
+            effect: 'flip',
+            loop: true,
+            hashNavigation: {
+                watchState: true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        })
+    })();
+
+    (function () {
+        var swipeInfo = document.querySelector('#swipe-info');
+        var swipeTimeout = setTimeout(function() {
+            swipeInfo.classList.add('slide-bottom');
+            setTimeout(function() {
+                swipeInfo.classList.remove('slide-bottom');
+            }, 8000);
+        }, 5000);
+    })();
+});
